22a64ea06dc1b1e57ab1f31d4179c6697e2f1d61
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-12 02:52:51 +0900

    웹 크롤링1

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var client = require('cheerio-httpcli');
 
 var url = "https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=1&acr=1&acq=%EB%83%89%EC%A0%95+%EB%A7%9B&qdt=0&ie=utf8&query=%EB%83%89%EC%A0%95+%EB%A7%9B%EC%A7%91"
 var param = {};
-
+ 
 client.fetch(url, param, function(err, $, res){
   if (err) {
     console.log("Error:", err);
